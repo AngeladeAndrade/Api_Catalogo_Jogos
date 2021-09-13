a89c3e5f39be25645d8983c09ed8e56b0445b192
AngeladeAndrade <anginhandrade@hotmail.com>
2021-09-12 21:41:00 -0300

    first commit

---
+++ ApiCatalogoJogos-main/DioCatalogoJogos.Api/InputModel/JogoInputModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DioCatalogoJogos.Api.InputModel
+{
+    public class JogoInputModel
+    {
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = " O nome do jogo deve conter 3 e 100 Caracteres")]
+
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = " O nome da produtora deve conter entre 3 e 100 Caracteres")]
+        public string Produtora { get; set; }
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = " O o preço deve ser fr no minimo 1 real e no maximo 1000 reais")]
+        public double Preco { get; set; }
+
+    }
+}
+
+
+
---
+++ ApiCatalogoJogos-main/DioCatalogoJogos.Api/Services/IJogoService.cs
@@ -0,0 +1,20 @@
+﻿using DioCatalogoJogos.Api.InputModel;
+using DioCatalogoJogos.Api.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DioCatalogoJogos.Api.Services
+{
+    public interface IJogoService: IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+        Task Inserir(JogoInputModel jogoInputModel);
+        Task Atualizar(Guid idJogo, JogoInputModel jogoInputModel);
+    }
+}
